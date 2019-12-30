19470b50dcff8a1fc9b2b776aa3293f9339f648e
diarmaid <diarmaiddeburca@gmail.com>
2019-12-30 02:46:10 +0100

    PEP8

--- tensorflow_load_model.py
+++ tensorflow_load_model.py
@@ -1,23 +1,30 @@
+"""
+Module holding test code to load a tensorflow model
+"""
 import tensorflow as tf
 import matplotlib.pyplot as plt
 import numpy as np
 
 
 def initial_function():
+    """
+    Initial function to load in a model
+    :return: None
+    """
     mnist = tf.keras.datasets.mnist
 
-    (xtrain, ytrain), (xtest, ytest) = mnist.load_data()
-    x_train = tf.keras.utils.normalize(xtrain, axis=1)
+    (_, _), (xtest, _) = mnist.load_data()
     x_test = tf.keras.utils.normalize(xtest, axis=1)
 
-    n = 22
-    plt.imshow(x_test[n], cmap=plt.cm.binary)
+    index_number = 22
+    plt.imshow(x_test[index_number], cmap=plt.cm.binary)  # pylint: disable=no-member
 
     new_model = tf.keras.models.load_model('epic_num_reader.model')
 
     predictions = new_model.predict(x_test)
-    print(np.argmax(predictions[n]))
+    print(np.argmax(predictions[index_number]))
     plt.show()
+
 
 if __name__ == "__main__":
     initial_function()
