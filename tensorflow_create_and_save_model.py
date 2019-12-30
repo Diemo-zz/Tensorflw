1db239ae5b0c247c9891b9e469591201dca1153e
diarmaid <diarmaiddeburca@gmail.com>
2019-12-30 02:45:31 +0100

    PEP8 stuff

--- tensorflow_create_and_save_model.py
+++ tensorflow_create_and_save_model.py
@@ -1,5 +1,7 @@
+"""
+Module holding the initial tensorflow stuff
+"""
 import tensorflow as tf
-import functools
 
 
 def test_function(length):
@@ -10,24 +12,32 @@ def test_function(length):
     if not isinstance(int, length):
         print("Not a proper lenght")
         return []
-    return [p for p in range(length)]
-#  asdf Break pep8 fdsa
+    return [1]*10
 
-mnist = tf.keras.datasets.mnist
-(xtrain, ytrain), (xtest, ytest) = mnist.load_data()
-x_train = tf.keras.utils.normalize(xtrain, axis=1)
-x_test = tf.keras.utils.normalize(xtest, axis=1)
-model = tf.keras.models.Sequential()
 
-model.add(tf.keras.layers.Flatten())
-model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu))
-model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu))
-model.add(tf.keras.layers.Dense(10, activation=tf.nn.softmax))
+def initial_function():
+    """
+    Initial setup function
+    :return: None
+    """
+    mnist = tf.keras.datasets.mnist
+    (xtrain, ytrain), (_, _) = mnist.load_data()
+    x_train = tf.keras.utils.normalize(xtrain, axis=1)
+    model = tf.keras.models.Sequential()
 
-model.compile(optimizer='adam',
-              loss='sparse_categorical_crossentropy',
-              metrics=['accuracy'])
+    model.add(tf.keras.layers.Flatten())
+    model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu))
+    model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu))
+    model.add(tf.keras.layers.Dense(10, activation=tf.nn.softmax))
 
-model.fit(x_train, ytrain, epochs=10)
+    model.compile(optimizer='adam',
+                  loss='sparse_categorical_crossentropy',
+                  metrics=['accuracy'])
 
-model.save('epic_num_reader.model')
+    model.fit(x_train, ytrain, epochs=10)
+
+    model.save('epic_num_reader.model')
+
+
+if __name__ == "__main__":
+    initial_function()
--- tensorflow_load_model.py
+++ tensorflow_load_model.py
@@ -2,17 +2,22 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 import numpy as np
 
-mnist = tf.keras.datasets.mnist
 
-(xtrain, ytrain), (xtest, ytest) = mnist.load_data()
-x_train = tf.keras.utils.normalize(xtrain, axis=1)
-x_test = tf.keras.utils.normalize(xtest, axis=1)
+def initial_function():
+    mnist = tf.keras.datasets.mnist
 
-n = 22
-plt.imshow(x_test[n], cmap=plt.cm.binary)
+    (xtrain, ytrain), (xtest, ytest) = mnist.load_data()
+    x_train = tf.keras.utils.normalize(xtrain, axis=1)
+    x_test = tf.keras.utils.normalize(xtest, axis=1)
 
-new_model = tf.keras.models.load_model('epic_num_reader.model')
+    n = 22
+    plt.imshow(x_test[n], cmap=plt.cm.binary)
 
-predictions = new_model.predict(x_test)
-print(np.argmax(predictions[n]))
-plt.show()
+    new_model = tf.keras.models.load_model('epic_num_reader.model')
+
+    predictions = new_model.predict(x_test)
+    print(np.argmax(predictions[n]))
+    plt.show()
+
+if __name__ == "__main__":
+    initial_function()
